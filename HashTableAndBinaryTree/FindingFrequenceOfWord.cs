84ddc736b5d5a355dc57ee493ba85a35d09bfb55
venubabu <velpurivenubabu123@gmail.com>
2023-05-15 07:51:51 +0530

    [Venubabu] Done with finding frequency

---
+++ HashTableAndBinaryTree/FindingFrequenceOfWord.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableAndBinaryTree
+{
+
+    internal class MyMapNode<K, V>
+    {
+        private LinkedList<KeyValuePair<K, V>>[] items;
+        private readonly int size;
+
+        public MyMapNode(int size)
+        {
+            items = new LinkedList<KeyValuePair<K, V>>[size];
+            this.size = size;
+        }
+
+        public int CountFrequency(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValuePair<K, V>> list = items[position];
+            int count = 1;
+            bool DoubleWord = false;
+            KeyValuePair<K, V> NewSentence = default(KeyValuePair<K, V>);
+            if (list != null)
+            {
+                foreach (KeyValuePair<K, V> pair in list)
+                {
+                    if (pair.key.Equals(key))
+                    {
+                        DoubleWord = true;
+                        NewSentence = pair;
+                        count = Convert.ToInt32(pair.value) + 1;
+                    }
+                }
+                if (DoubleWord == true)
+                {
+                    list.Remove(NewSentence);
+                }
+            }
+            return count;
+        }
+
+        public int GetArrayPosition(K Key)
+        {
+            var position = Key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+
+
+        public void Add(K Key, V Value)
+        {
+            int position = GetArrayPosition(Key);
+            LinkedList<KeyValuePair<K, V>> linkedList = GetLinkedList(position);
+            KeyValuePair<K, V> Adding = new KeyValuePair<K, V>() { key = Key, value = Value };
+            linkedList.AddLast(Adding);
+        }
+
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValuePair<K, V>> linkedList = items[position];
+            KeyValuePair<K, V> Removing = default(KeyValuePair<K, V>);
+            foreach (KeyValuePair<K, V> kv in linkedList)
+            {
+                if (kv.key.Equals(key))
+                {
+                    Removing = kv;
+                }
+            }
+            linkedList.Remove(Removing);
+        }
+
+        public LinkedList<KeyValuePair<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValuePair<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValuePair<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+
+        public void Display(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValuePair<K, V>> LinkedListofPosition = GetLinkedList(position);
+            foreach (KeyValuePair<K, V> keyValue in LinkedListofPosition)
+            {
+                if (keyValue.key.Equals(key))
+                {
+                    Console.WriteLine($"{keyValue.key} : {keyValue.value}");
+                }
+            }
+        }
+    }
+
+    public struct KeyValuePair<K, V>
+    {
+        public K key { get; set; }
+        public V value { get; set; }
+    }
+}
--- HashTableAndBinaryTree/Program.cs
+++ HashTableAndBinaryTree/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace HashTableAndBinaryTree
+﻿using HashTableAndBinaryTree;
+
+namespace HashTable
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! Welcome To HashTable And Binary Tree");
+            Console.WriteLine("Hash Table");
+            String[] Sentence = { "TO", "BE", "OR", "NOT", "TO", "BE" };
+            WordFrequency(Sentence);
+        }
+
+
+        public static void WordFrequency(string[] Sentence)
+        {
+            MyMapNode<string, int> MyMapNode = new MyMapNode<string, int>(5);
+            foreach (string word in Sentence)
+            {
+                int count = MyMapNode.CountFrequency(word);
+                MyMapNode.Add(word, count);
+            }
+            IEnumerable<string> DistinctSentence = Sentence.Distinct<string>();
+            foreach (string word in DistinctSentence)
+            {
+                MyMapNode.Display(word);
+            }
         }
     }
 }
